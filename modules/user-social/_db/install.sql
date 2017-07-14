3a04bc241e0c1e500a5a83ad891206a94c8c0ec5
iqbalfn <iqbalfawz@gmail.com>
2017-07-14 10:55:32 +0700

    Remove table partition

--- modules/user-social/_db/install.sql
+++ modules/user-social/_db/install.sql
@@ -1,15 +1,8 @@
 CREATE TABLE IF NOT EXISTS `user_social` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `user` INTEGER NOT NULL,
     `vendor` VARCHAR(50) NOT NULL,
     `vuid` VARCHAR(100),
     `page` VARCHAR(150),
-    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
-    PRIMARY KEY(
-        `id`,
-        `vendor`
-    )
-)
-    PARTITION BY KEY(`vendor`)
-    PARTITIONS 50;
+    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
